930a6973156a4129c6143ebb636a312b609ee155
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-02-04 16:33:34 -0300

    viewBindingUndertanding

---
+++ app/src/main/java/com/example/viewbindingstructure/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.viewbindingstructure
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.viewbindingstructure.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var contador: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) //pegando a raiz do xml- contraintLayout da activity_main
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        this.binding.btnContador.setOnClickListener {
+            contador++ 
+            this.binding.tvContador.text = "Contador $contador"
+        }
+
+    }
+}
