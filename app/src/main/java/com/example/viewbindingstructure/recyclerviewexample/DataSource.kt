1fb2e93fc1c98a71f9f508cdbebd1211fe28ca5a
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-02-08 09:39:56 -0300

    recyclerViewOnViewBinding

--- app/src/main/java/com/example/viewbindingstructure/recyclerviewexample/DataSource.kt
+++ app/src/main/java/com/example/viewbindingstructure/recyclerviewexample/DataSource.kt
@@ -11,50 +11,42 @@ class DataSource {
             val list = ArrayList<Live>()
             list.add(
                 Live(
-                    "Sword Art Online - Progressive",
-                    "Guga Padilha",
-                    "https://sucodemanga.com.br/wp-content/uploads/2021/11/sao-progressive-capa.webp",
-                    "https://www.youtube.com/watch?v=59knIuqQ1rU&ab_channel=Matt"
+                    "Archive 81",
+                    "Rebecca Sonnenshine",
+                    "https://epipoca.com.br/wp-content/uploads/2022/01/Arquivo-81-critia.jpeg",
+                    "https://www.youtube.com/watch?v=ibxKEqxARkE&ab_channel=Netflix"
                 )
             )
             list.add(
                 Live(
-                    "Sword Art Online - Progressive",
-                    "Guga Padilha",
-                    "https://sucodemanga.com.br/wp-content/uploads/2021/11/sao-progressive-capa.webp",
-                    "https://www.youtube.com/watch?v=59knIuqQ1rU&ab_channel=Matt"
+                    "All of us are dead",
+                    "Lee Jae-kyoo;",
+                    "https://img.ibxk.com.br/2022/02/04/04144437005231.jpg",
+                    "https://www.youtube.com/watch?v=bmbl65oiUgA&ab_channel=NetflixBrasil"
                 )
             )
             list.add(
                 Live(
-                    "Sword Art Online - Progressive",
+                    "The Witcher - Season 2",
                     "Guga Padilha",
-                    "https://sucodemanga.com.br/wp-content/uploads/2021/11/sao-progressive-capa.webp",
-                    "https://www.youtube.com/watch?v=59knIuqQ1rU&ab_channel=Matt"
+                    "https://files.tecnoblog.net/wp-content/uploads/2020/01/the-witcher-netflix.jpg",
+                    "https://www.youtube.com/watch?v=OtLG_mWdZZg&ab_channel=NetflixBrasil"
                 )
             )
             list.add(
                 Live(
-                    "Sword Art Online - Progressive",
-                    "Guga Padilha",
-                    "https://sucodemanga.com.br/wp-content/uploads/2021/11/sao-progressive-capa.webp",
-                    "https://www.youtube.com/watch?v=59knIuqQ1rU&ab_channel=Matt"
+                    "Peaky Blinders - Final Season",
+                    "Steven Knighta",
+                    "https://tm.ibxk.com.br/2020/11/19/19150622444261.jpg?ims=1200x675",
+                    "https://www.youtube.com/watch?v=2nsT9uQPIrk&ab_channel=BBC"
                 )
             )
             list.add(
                 Live(
-                    "Sword Art Online - Progressive",
-                    "Guga Padilha",
-                    "https://sucodemanga.com.br/wp-content/uploads/2021/11/sao-progressive-capa.webp",
-                    "https://www.youtube.com/watch?v=59knIuqQ1rU&ab_channel=Matt"
-                )
-            )
-            list.add(
-                Live(
-                    "Sword Art Online - Progressive",
-                    "Guga Padilha",
-                    "https://sucodemanga.com.br/wp-content/uploads/2021/11/sao-progressive-capa.webp",
-                    "https://www.youtube.com/watch?v=59knIuqQ1rU&ab_channel=Matt"
+                    "The Sinner - Final Season",
+                    "Derek Simonds",
+                    "https://img.ibxk.com.br/2021/11/17/17225640868012.jpg",
+                    "https://www.youtube.com/watch?v=kFJbvqZ609w&ab_channel=TVPromos"
                 )
             )
 
